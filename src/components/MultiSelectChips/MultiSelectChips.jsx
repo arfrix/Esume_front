395c00c80221bdec74c301002f71058a2a78a6ff
arfa <arfrix.dev@gmail.com>
2021-02-24 20:27:39 +0330

    created but not tested

--- src/components/MultiSelectChips/MultiSelectChips.jsx
+++ src/components/MultiSelectChips/MultiSelectChips.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Chip from '@material-ui/core/Chip';
 
-export default function MultiSelectChips() {
+
+export default function MultiSelectChips(chipsData) {
+
+  const [chipsList, setChipsList] = useState(chipsData)
+
+  const deleteChips = (targetChipKey) => {
+    setChipsList((chipsList) => chipsList.filter((chip) => chip.key !== targetChipKey))
+  }
+
+
   return (
     <div>
+      {chipsList.map((chip) => {
+
+        return(
+          <li key={chip.key}>
+            <chip
+              label={chip.label}
+              onDelete={deleteChips(chip.key)}
+            />
+          </li>
+        )
+      })}
       
     </div>
   )
