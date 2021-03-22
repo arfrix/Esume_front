08b9afae86fccd4568a310ce7737a49aff8e52aa
arfa <arfrix.dev@gmail.com>
2021-03-22 11:53:29 +0430

    text and style added

--- src/components/itemAdder/ItemAdder.jsx
+++ src/components/itemAdder/ItemAdder.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
-
+import TextField from '@material-ui/core/TextField';
+import './ItemAdder.scss'
 
 export default function ItemAdder() {
   const [stack, setstack] = useState(null)
   return (
-    <div>
+    <div className="ItemAdder__main">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
       <Button variant="contained" color="primary">
         ثبت
       </Button>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,6 +3,7 @@ import ListItems from "../components/ListItems";
 import StepManager from "../components/stepManager/StepManager"
 import SkillLevel from "../views/SkillLevel"
 import TechStackSelection from "../views/TechStackSelection"
+import ItemAdder from "../components/itemAdder/ItemAdder"
 
 export default function Home() {
   const config = [
@@ -14,6 +15,9 @@ export default function Home() {
     return (
         <div>
             <StepManager currentStep={1} childComponentOrder={1}>
+              <ItemAdder />
+            </StepManager>
+            <StepManager currentStep={1} childComponentOrder={3}>
               <SkillLevel hasDatePicker />
             </StepManager>
             <StepManager currentStep={1} childComponentOrder={2}>
